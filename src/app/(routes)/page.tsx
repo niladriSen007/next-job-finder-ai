0f1258525c5388de147b6148acfd8025fc656969
Niladri <niladrisengit12@gmail.com>
2025-07-12 11:58:17 +0530

    feat: implement DashboardPage and Home component with responsive design

--- src/app/(routes)/page.tsx
+++ src/app/(routes)/page.tsx
@@ -1,6 +1,11 @@
+import Home from "@/components/shared/homepage"
+
 const DashboardPage = () => {
   return (
-    <div>DashboardPage</div>
+    <div className="h-screen text-white mt-24">
+      Hii
+      <Home />
+    </div>
   )
 }
 export default DashboardPage
